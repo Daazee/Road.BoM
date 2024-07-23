ebd834879a1fc71e2329f450c67c5701743ff454
Azeez Adebayo <azeez.adedayo@yahoo.com>
2024-07-23 21:59:52 +0300

    Add settings controller

---
+++ src/RoadBoM.Web/Controllers/SettingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoadBoM.Web.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult BillCategories()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/RoadBoM.Web/Models/ViewModels/BillCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace RoadBoM.Web.Models.ViewModels
+{
+    public class BillCategoryViewModel
+    {
+        public long Id { get; set; }
+        public string Code { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Order { get; set; }
+        public int Status { get; set; }
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+        public string UpdatedBy { get; set; } = null!;
+        public DateTime UpdatededOn { get; set; }
+
+    }
+}
